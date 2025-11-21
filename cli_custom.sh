e706b5162ea860a217d16664717108ef6e3cd20c
lucasgrjn <18169686+lucasgrjn@users.noreply.github.com>
2025-11-20 21:37:39 -0800

    dev: add quick and dirty cli_custom.sh

---
+++ cli_custom.sh
@@ -0,0 +1,16 @@
+brew install harper
+brew install --cask ghostty
+brew install helix
+brew install yazi
+brew install broot
+brew install eza
+brew install htop
+brew install tokei
+brew install gping
+brew install bat
+brew install procs
+berw install gitui
+brew install hyperfine
+brew install choose-rust
+brew install zoxide
+brew install fzf
